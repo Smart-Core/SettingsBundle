03294334d5e4aa9936e12e4ac6e71f015cef74a3
Smart-Core <artem.digi@gmail.com>
2015-06-26 09:48:31 +0600

    fix

--- DependencyInjection/SmartSettingsExtension.php
+++ DependencyInjection/SmartSettingsExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SmartCoreSettingsExtension extends Extension
+class SmartSettingsExtension extends Extension
 {
     /**
      * {@inheritdoc}
