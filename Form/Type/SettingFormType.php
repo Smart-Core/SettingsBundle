8e0968c87c404ec98a1ca8a3f24ad439e798d77c
Smart-Core <artem.digi@gmail.com>
2015-07-04 18:31:54 +0600

    change OptionsResolverInterface  to OptionsResolver

--- Form/Type/SettingFormType.php
+++ Form/Type/SettingFormType.php
@@ -5,7 +5,7 @@ namespace SmartCore\Bundle\SettingsBundle\Form\Type;
 use Smart\CoreBundle\Form\DataTransformer\HtmlTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SettingFormType extends AbstractType
 {
@@ -19,7 +19,7 @@ class SettingFormType extends AbstractType
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => 'SmartCore\Bundle\SettingsBundle\Entity\Setting',
